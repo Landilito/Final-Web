f8348a8f963f962124f41340a45a2f1708df18e8
Landilito <36903709+Landilito@users.noreply.github.com>
2019-12-05 18:09:32 -0400

    Creacion de los modelos para las entidades de la base de datos

---
+++ FinalWeb/Models/Socios.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinalWeb.Models
+{
+    public class Socios
+    {
+        [Key]
+        public int SociosID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+        public int Telefono { get; set; }
+        public string Sexo { get; set; }
+        public int Edad { get; set; }
+        public DateTime Nacimiento { get; set; }
+        public Afiliado Afiliados { get; set; }
+        public Membresia Membresia { get; set; }
+        public string LugarTrabajo { get; set; }
+        public string DireccionOficina { get; set; }
+        public string TelefonoOficina { get; set; }
+        public Estado Estado { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public DateTime FechaSalida { get; set; }
+    }
+
+    public enum Afiliado
+    {
+        Madre, Padre, Pareja, Hijos, Otros
+    }
+
+    public enum Membresia
+    {
+        Basic, Silver, Gold, Black
+    }
+
+    public enum Estado
+    {
+        Activo, Inactivo
+    }
+}
